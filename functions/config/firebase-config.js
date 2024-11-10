46a45ab6e7ad42dab89e9ef6fff4942b8cff9e10
jyassien <jihadumuhammed@gmail.com>
2024-11-09 17:57:52 -0700

    basic calls and examples to plantID.

--- functions/config/firebase-config.js
+++ functions/config/firebase-config.js
@@ -3,7 +3,7 @@ const admin = require("firebase-admin");
 const serviceAccountObj = require("./serviceAccountKeys");
 // const serviceAccountObj = require("./serviceAccountKeys.json");
 
-console.log("Service account keys:", Object.keys(serviceAccountObj));
+// console.log("Service account keys:", Object.keys(serviceAccountObj));
 // console.log("private_key :", serviceAccountObj.private_key);
 
 // Initialize Firebase Admin SDK with the service account
